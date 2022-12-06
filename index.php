5cb27ac886fca8745652a213db4e1c624082c8ec
Mohamed Ali <mohamedalimas912@gmail.com>
2022-12-06 21:05:20 +0200

    commit

--- index.php
+++ index.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Library - PHP</title>
 </head>
 <body>
+    <h1>Library - PHP - Project</h1>
+    <?php
+    $namee = "moooo";
+    echo "<h1>Hello  $namee </h1>";
+    echo "<br>";
+    echo "Hello There again!!.";
     
+    ?>
+    <?php
+    $num = 5;
+    $name = "Mohamed";
+    echo "<h1>Hello $name </h1>";
+    ?>
 </body>
 </html>
